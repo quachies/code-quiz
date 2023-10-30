c27bfc294b7e52c356f3042bba05808f7b434eaa
Kevin Quach <kevinquach@kevins-mbp.home>
2023-10-29 23:27:42 -0400

    Added CSS code and fixed up button centering

--- script.js
+++ script.js
@@ -3,51 +3,54 @@ let opEl = document.getElementById("options");
 let resultEl = document.getElementById("results");
 let scoreEl = document.getElementById("score");
 let timerEl = document.getElementById("timer");
+let playerEl = document.getElementById("players")
+let startButton = document.getElementById("start-button")
+let resetButton = document.getElementById("reset-button")
 
 let questions = [
     {
-        question: "question 1",
+        question: "Commonly used data types do not include:",
         option: [
-            { text: "A", correct: false },
-            { text: "A", correct: false },
-            { text: "A", correct: false },
-            { text: "A", correct: true }
+            { text: "strings", correct: false },
+            { text: "booleans", correct: false },
+            { text: "numbers", correct: false },
+            { text: "alerts", correct: true }
         ]
     },
     {
-        question: "question 2",
+        question: "The condition in an if / else statement is enclosed within __.",
         option: [
-            { text: "A", correct: false },
-            { text: "A", correct: true },
-            { text: "A", correct: false },
-            { text: "A", correct: false }
+            { text: "quotes", correct: false },
+            { text: "parentheses", correct: true },
+            { text: "curly brackets", correct: false },
+            { text: "square brackets", correct: false }
         ]
     },
     {
-        question: "question 3",
+        question: "Arrays in Javascript can be used to store",
         option: [
-            { text: "A", correct: false },
-            { text: "A", correct: true },
-            { text: "A", correct: false },
-            { text: "A", correct: false }
+            { text: "numbers and strings", correct: false },
+            { text: "booleans", correct: false },
+            { text: "other arrays", correct: false },
+            { text: "all the above", correct: true }
         ]
     },
     {
-        question: "question 4",
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         option: [
-            { text: "A", correct: false },
-            { text: "A", correct: false },
-            { text: "A", correct: true },
-            { text: "A", correct: false }
+            { text: "JavaScript", correct: false },
+            { text: "terminal/bash", correct: false },
+            { text: "for loops", correct: false },
+            { text: "console.log", correct: true }
         ]
     },
     {
-        question: "question 5",
+        question: "What element is used for displaying images on a web page?",
         option: [
-            { text: "A", correct: false },
-            { text: "A", correct: false },
-            { text: "A", correct: false },
-            { text: "A", correct: true }
+            { text: "<img>", correct: true },
+            { text: "<div>", correct: false },
+            { text: "<body>", correct: false },
+            { text: "<nav>", correct: false }
         ]
     },
 ]
@@ -66,6 +69,7 @@ function showQuestion() {
         optionButton.classList.add("option")
         optionButton.addEventListener("click", () => checkAnswer(option.correct))
         opEl.appendChild(optionButton)
+        resultEl.textContent = ""
     })
 
 }
@@ -80,13 +84,15 @@ function showNextQuestion() {
 function checkAnswer(isCorrect) {
     if (isCorrect) {
         resultEl.textContent = "correct"
-        setTimeout(showNextQuestion, 1000)
+        setTimeout(showNextQuestion, 500)
         score++
-        scoreEl.textContent = score
+        scoreEl.textContent = "Current score: " + score
         if (currentQuestionIndex === questions.length - 1) { endQuiz() }
     } else{
         resultEl.textContent = "incorrect (-5s)"
         timeLeft -= 5
+        setTimeout(showNextQuestion, 500)
+        if (currentQuestionIndex === questions.length - 1) { endQuiz() }
     }
 }
 
@@ -103,4 +109,24 @@ function timer() {
     timeLeft --;
     timerEl.textContent = "Time Left " + timeLeft + "s" 
 }
+
+function endQuiz() {
+    let highscore = JSON.parse(localStorage.getItem("player")) || {}
+    clearInterval(timerInterval)
+    timerEl.textContent = "Game Over" 
+    let initial = prompt("input initials, score: " + score)
+    highscore[initial] = score
+    localStorage.setItem("player", JSON.stringify(highscore))
+    playerEl.textContent = "HighScores: " + localStorage.getItem("player")
+}
+
+function resetQuiz() {
+    clearInterval(timerInterval)
+    timeLeft = 60
+    timerEl.textContent = "Time Left " + timeLeft + "s"
+    window.location.reload()
+}
+
+startButton.addEventListener("click", startQuiz);
+resetButton.addEventListener("click", resetQuiz);
 
